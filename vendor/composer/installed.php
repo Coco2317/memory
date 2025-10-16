3a60d34e24a796d97d85cb8d08ab9c6f7c48404a
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-10-16 14:02:45 +0200

    autoload mis en place

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nicole/memory',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5534507d97d1e22f4bd667f41ad8356294f3c1ad',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nicole/memory' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5534507d97d1e22f4bd667f41ad8356294f3c1ad',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
